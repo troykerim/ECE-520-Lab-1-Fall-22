8b2aa4753c5903e0858713d4c1948f53d8bbb88d
Troy Israel <31581632+troykerim@users.noreply.github.com>
2023-03-28 21:22:32 -0700

    Adding files from Lab 1
    Adding my files from Lab 1 to make them public

---
+++ fa22-e520-lab1-troykerim/software/LED_test/src/led_test.c
@@ -0,0 +1,54 @@
+/*Include Files*/
+#include "xparameters.h"
+#include "xgpio.h"
+#include "xstatus.h"
+#include "xil_printf.h"
+
+/* Definitions */
+#define GPIO_DEVICE_ID XPAR_AXI_GPIO_0_DEVICE_ID
+#define LED 0x9
+#define LED_DELAY 10000000
+#define LED_CHANNEL 1
+#define printf xil_printf
+
+XGpio Gpio;
+
+int LEDOutputExample(void)
+{
+	volatile int Delay;
+	int Status;
+	int led = LED;
+
+	Status = XGpio_Initialize(&Gpio, GPIO_DEVICE_ID);
+	if (Status != XST_SUCCESS)
+	{
+		return XST_FAILURE;
+	}
+
+	XGpio_SetDataDirection(&Gpio, LED_CHANNEL, 0x0);
+	while(1)
+	{
+		XGpio_DiscreteWrite(&Gpio,LED_CHANNEL,led);
+		led = ~led;
+
+		for (Delay = 0; Delay < LED_DELAY; Delay++);
+	}
+	return XST_SUCCESS;
+}
+
+/* Main Function */
+int main(void)
+{
+	int Status;
+	/*Execute the LED output. */
+	Status = LEDOutputExample();
+	if (Status != XST_SUCCESS)
+	{
+		xil_printf("GPIO output to the LEDs failed!\r\n");
+	}
+	return 0;
+}
+
+
+
+
